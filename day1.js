368f4f285071f504ad0a2e450e497a78970a4601
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-01 20:56:12 +0600

    about script tag

--- day1.js
+++
@@ -1,3 +0,0 @@
-console.log(
-  "Hello world It's 2024. InshaAllah I will write code daily from now on"
-);
---
+++ js-fundamentals.js
@@ -0,0 +1,2 @@
+// why we put <script> tag in the end of HTML file?
+//* We can insert <script> tag almost anywhere into an HTML file. The JavaScript code inside the <script> tag execute automatically when the browser process the tag. So if we use an HTML element in the script then the script cant access that element if that is below the <script> tag
