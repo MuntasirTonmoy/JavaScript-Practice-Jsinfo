e8dcedf39fe8fb63f685c45a28d844afa5211da6
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-01 21:23:14 +0600

    question ans added

--- js-fundamentals.js
+++ js-fundamentals.js
@@ -1,2 +1,6 @@
 // why we put <script> tag in the end of HTML file?
 //* We can insert <script> tag almost anywhere into an HTML file. The JavaScript code inside the <script> tag execute automatically when the browser process the tag. So if we use an HTML element in the script then the script cant access that element if that is below the <script> tag
+
+// How to run Js code
+//* in nodejs use the command "node filename.js", we can ommit .js extension as node.js by default search file with .js extension
+//* in browser we can insert JavaScript code into an HTML file using <script> tag
