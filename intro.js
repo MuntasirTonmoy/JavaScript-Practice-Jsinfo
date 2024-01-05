ddf842ba977fd4d84e5bad1e6c91f38221a50e0c
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-05 09:45:00 +0600

    into file added

---
+++ intro.js
@@ -0,0 +1,23 @@
+// why JS was initially created?
+// what is the program in this language called?
+
+// where we can execute js code?
+
+// How do the engines works?
+
+// what can in-browser js do?
+
+// what can't in-browser js do?
+//* access files in hard disk without user permission
+//* same origin policy
+//* receive data from other domain is crippled
+
+// what make js unique?
+// language over javascript
+
+// specification, manuals, compatibility table for javascript
+
+// IDE vs Lightweight editor
+
+// Where can we see errors on the browser?
+// how to write multiple line of code in developer console?
