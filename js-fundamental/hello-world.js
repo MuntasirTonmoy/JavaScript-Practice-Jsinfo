c8160e7436c90f077885207f347a880b0bd256de
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-05 09:53:06 +0600

    hello world file done

---
+++ js-fundamental/hello-world.js
@@ -0,0 +1,12 @@
+// how to run js code in node.js?
+// where can we put <script> tag and how the js program inside the <script> tag execute?
+
+// type attribute in <script> tag. what does it means now? in HTML4 why does the type attribute is being used?
+// why does language attribute is used?
+
+// why in old time this technique is used <script type="text/javascript"><!--//--></script>
+
+// when we use external js script and is there any benifit?
+// what type can be the path in src='' attribute
+
+// if the src='' attribute is set does the program inside the <scrip> tag worked or ignored?
