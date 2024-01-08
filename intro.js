fc05af16f137d46196500d70c8d9672d6050b84c
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-08 10:27:24 +0600

    edited

--- intro.js
+++ intro.js
@@ -1,23 +1,22 @@
 // why JS was initially created?
 // what is the program in this language called?
-
 // where we can execute js code?
-
 // How do the engines works?
-
 // what can in-browser js do?
-
+// webpage manipulation, interaction with the user, and the webserver.
 // what can't in-browser js do?
-//* access files in hard disk without user permission
-//* same origin policy
-//* receive data from other domain is crippled
-
-// what make js unique?
+//- access files in hard disk without user permission
+//- same origin policy
+//- receive data from other domain is crippled
+// what make js unique? full integration with hc
 // language over javascript
 
+//* useful links
 // specification, manuals, compatibility table for javascript
 
+//* ide
 // IDE vs Lightweight editor
 
+//* developer console
 // Where can we see errors on the browser?
 // how to write multiple line of code in developer console?
