748f30c06c73ce8db8c74ac6fc5b4dda346ed825
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-09 20:45:17 +0600

    data-type

---
+++ js-fundamental/data-type.js
@@ -0,0 +1,36 @@
+// is js static type language or dynamically typed?
+// how many data type are there in js?
+
+// ===PRIMITIVE===
+
+//* Number
+// number data type represent what values?
+// what are Infinity, -Infinity, NaN? they belong to what type?
+// Any further math operation with NaN returns what? is there any exception?
+
+//* BigInt
+// what is bigint data type? why we need them?
+
+//* String
+// how to declare string?
+// how many type of quotes can we use to declare string in js?
+// what is template literals or template string?
+
+//* Boolean
+// what are the boolean type value?
+
+//* Null
+// what is null type value represent? does is represent null pointer or empty object?
+// what is the typeof null?
+
+//* Undefined
+// what is undefined type in js?
+
+//* Symbol
+// what is symbol type value used?
+
+// ===Non Primitive===
+//* Object
+
+// how can we know the data type of a value?
+// typeof(x) what does (x) represent? is it a function?
