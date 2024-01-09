75a96a99946225a9639f3bc31668914391e5f620
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-09 23:20:41 +0600

    comparison

---
+++ js-fundamental/operators/comparison.js
@@ -0,0 +1,7 @@
+// what are comparison operators?
+// all comparison operator return what? can we assign the return result to a variable?
+// how string comparison done?
+// when comparing different type of values on what type javascript convert them automatically?
+// difference between regular equality check vs strict equality check?
+// how null and undefined comparison works for == and ===?
+// can we compare undefined with other type of value?
