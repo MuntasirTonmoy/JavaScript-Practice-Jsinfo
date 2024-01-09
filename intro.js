0342561476efa17d6f6d995fc59fbd919fa25b40
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-09 20:30:29 +0600

    variable

--- intro.js
+++ intro.js
@@ -2,11 +2,16 @@
 // what is the program in this language called?
 // where we can execute js code?
 // How do the js engines works?
-// what can in-browser js do? webpage manipulation, interaction with the user, and the webserver.
+// what can in-browser js do?
+
+//webpage manipulation, interaction with the user, and the webserver.
+
 // what can't in-browser js do?
+
 //- access files in hard disk without user permission
-//- same origin policy
-//- receive data from other domain is crippled
+//- same origin policy, Different tabs/windows generally do not know about each other,JavaScript from one page may not access the other page if they come from different sites (from a different domain, protocol or port).
+//- receive data from other site/domain is crippled
+
 // what make js unique? full integration with hc
 // language over javascript
 
---
+++ js-fundamental/variable.js
@@ -0,0 +1,14 @@
+// what is variable?
+// how to declare a variable? and then access it?
+// how to assign value in a variable?
+// can we combine the declaration and assignment at the same time?
+// how to declare and assign multiple value in one line? should we do it?
+// if we declare the same name variable twice will it cause error?
+// variable naming rule? there are 3
+// In js what case we use to write variable name?
+// if we assign a value without let keyword? is it an error?
+
+// How to declare a constant in js?
+// when we use UPPERCASE CONSTANT?
+
+// When naming variable what should we consider? and what are the some rules to follow when naming variable?
