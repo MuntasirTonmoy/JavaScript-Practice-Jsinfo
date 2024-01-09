e834fa80df22fbc7a4f955f457c5765a025ec7f1
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-09 21:35:17 +0600

    type-conversion

---
+++ js-fundamental/type-conversion.js
@@ -0,0 +1,10 @@
+// what is implicit and explicit type conversion?
+
+// give example
+// how convert data from other type to string type explicitly?
+// how convert data from other type to number type explicitly?
+// how convert data from other type to boolean type explicitly?
+
+// give example
+// what are the numeric type conversion rules?
+// what are the boolean type conversion rules?
