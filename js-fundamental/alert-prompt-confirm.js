644274925d1acbe642b437bcc249de31d0e16729
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-09 20:50:07 +0600

    browser funtion

---
+++ js-fundamental/alert-prompt-confirm.js
@@ -0,0 +1,7 @@
+// what does alert() do in browser?
+
+// what does prompt('title',[default]) do in browser? if we provide default value what happened?
+// if user ignore the prompt what value it returns?
+
+// what does confirm() use for in browser?
+// what does confirm() return if user interact with it?
