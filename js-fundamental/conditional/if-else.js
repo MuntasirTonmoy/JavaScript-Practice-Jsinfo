d0d90c6fcbff8f2c270d91d100dbbadd1c5dccc7
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-10 11:37:18 +0600

    if-else

---
+++ js-fundamental/conditional/if-else.js
@@ -0,0 +1,5 @@
+// how if statement works? what type of value does it check?
+// does if statement convert data type implicitly?
+
+// for one line if statement do we need to wrap it with bracket?
+// when we use else if and else caluse?
---
+++ js-fundamental/operators/conditional.js
@@ -0,0 +1,6 @@
+// what is conditional operator?
+// why it is called ternary?
+// can we chain conditional operator?
+// what is the purpose of conditional operator?
+
+// should we replace if statement with conditional operator?
