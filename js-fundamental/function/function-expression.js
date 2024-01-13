e4096c1ed99673f19aeaa44714bdbd269b13a051
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-13 12:36:52 +0600

    loop

---
+++ js-fundamental/function/function-expression.js
@@ -0,0 +1,12 @@
+// what is function expression in js?
+// in js is function a value? how?
+
+// should we put semicolon after function expression?
+
+// what is callback function?
+
+// what is anonymous function?
+// function expression vs function declaration?
+// is function declaration block scope?
+
+// when to use what, function expression vs function declaration
