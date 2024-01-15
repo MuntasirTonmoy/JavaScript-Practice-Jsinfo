792623fc44c8d1ca36f5701b3564827ff2c4c29c
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-15 09:40:25 +0600

    object

---
+++ object/object.js
@@ -0,0 +1,24 @@
+// object are used to store what?
+// how an object can be created?
+// what is object literal syntax, object constructor syntax?
+// what is a property? what type property value can be?
+// what is trailing or hanging comma?
+// how can we access a property value?
+// How to remove a property?
+
+// can we use multiword property name? how?
+// how can we access multiword property value?
+// why we can't use dot notation when accessing multiword property? what is the rules for using dot notation?
+//can we use an expression when using square bracket notation?
+// what is computed property?
+
+// what is property value shorthand?
+
+// is there any limitation at property name?
+// how to check if a property exist or not?
+// on the left side of in what must be there? why we use in operator instead of checking undefined?
+
+// how to use for-in loop
+
+// how object properties are ordered by default?
+// what does integer properties mean?
