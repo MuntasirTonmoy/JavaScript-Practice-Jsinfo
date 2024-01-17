e4509ddc32d1317603712c8a5fd09e2107787d1b
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-17 10:52:44 +0600

    object-copy

---
+++ object/object-copy-reference-copy.js
@@ -0,0 +1,9 @@
+// how object are stored and copied? how primitive are stored and copied?
+// a variable assigned to an object store what? when we perform an action with the object what does js engine does?
+// when two objects are equal?
+// can we modify an object declared by constant?
+// how to clone an merge an object?
+// how object.assign work? if the copied property name already exist does it gets overwritten?
+// how can we clone a nested object? what does deep clone or structured clone mean?
+// how structuredClone() work? does it support circular ref? does it clone method?
+// what function loadash library provide for deep clone?
