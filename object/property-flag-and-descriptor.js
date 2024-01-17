e4cb29dde816debda3019a6e593bd54348a567df
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-17 16:21:26 +0600

    object property

---
+++ object/property-flag-and-descriptor.js
@@ -0,0 +1,11 @@
+// what is property flags? how many?
+// when we create an object by default all the flags value are what?
+// how can we get full information about an object property?
+// how can we change these flags value?
+// when we use object.defineProperty to create a new property if we don't supply a flag value then what is the default value?
+// if a property already exist and we change a single flag value and don't supply other flag will they become false?
+// what does non writable, nor enumerable and non configurable mean?
+// how can we create multiple properties with flags? and how can we get multiple property descriptors?
+
+// how can we seal, prevent extension, freeze an entire object?
+// how can we check an object is freeze, sealed, extensible or not?
