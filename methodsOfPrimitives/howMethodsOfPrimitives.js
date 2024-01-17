1ceaf50ab5e42363b00b086aa36712c34e6bb57c
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-17 16:02:13 +0600

    methods-of-primitive

---
+++ methodsOfPrimitives/howMethodsOfPrimitives.js
@@ -0,0 +1,3 @@
+// how methods of primitive works?
+// should we use Number/Boolean/String constructor to create these value?
+// does null and undefined have methods?
---
+++ object/object-to-primitive.js
@@ -0,0 +1,8 @@
+// do object convert to boolean?
+// when numeric and when mathematical conversion happened?
+// how does js decide when to do what conversion?
+// what is hints? how many hints are there?
+// to do the conversion js called what 3 methods?
+
+// how do we implement these methods?
+// what these method must return?
