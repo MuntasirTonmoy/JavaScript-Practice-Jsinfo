a2cec70c0da0b9b1a3c2b57ace85e69e30a8a699
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-17 21:32:18 +0600

    prototype

---
+++ object/prototype-inheritence/protype.js
@@ -0,0 +1,20 @@
+// in js every object has a special hidden property what is it?
+// that value of [[Prototype]] can be what?
+// if the value of [[Prototype]] is an object what does it called?
+// if we read a prop from an obj and it's missing javascript does what?
+
+// how can we get/set the[[Prototype]] property?
+// what is inherited prop?
+
+// what is the limitation when creating prototype chain?
+// what is __proto__?
+
+// does the prototype is only for reading prop?
+// does writing property in object affect prototype?
+// if we write on an accessor property does it affect prototype?
+
+// is the value of this affected by prototype?
+
+// for in loop iterate over inherited prop also how can we stop it if we don't want it?
+
+// does all other key value getting method ignore inherited prop?
