f86a419a9908b757c7713f7b6193889d2c95d148
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2024-01-18 23:23:18 +0600

    js constructor function prototype

---
+++ object/prototype-inheritence/prototype-in-constructor.js
@@ -0,0 +1,9 @@
+// in old days how prototype was used in js?
+// If we add a 'prototype' property in a constructor function what does that mean?
+// how we set prototype of a newly created object via constructor function?
+// when only F.prototype is used?
+// is there default prototype property in constructor function? what value does it have?
+// how we can use default prototype object constructor property?
+
+// if we replace the default prototype then what happened?
+// how should we use prototype so the constructor property doesn't change?
