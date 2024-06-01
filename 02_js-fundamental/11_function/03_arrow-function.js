0b842817c87788a172ec25429370869a92feeb71
Tonmoy <muntasir01521566317@gmail.com>
2024-06-01 23:42:12 +0600

    edited

--- 02_js-fundamental/11_function/03_arrow-function.js
+++ 02_js-fundamental/11_function/03_arrow-function.js
@@ -4,6 +4,8 @@
 
 // must we use return keyword inside curly braces for multiline arrow function?
 
+// can we use 'arguments' inside an arrow function? if we use 'arguments' in an arrow function the value is taken from where?
+
 // does arrow function have 'this' keyword? how the value of 'this' keyword determines in arrow function?
 
 // why arrow function can't be used as constructor function?
---
+++ function_advance/02_rest_param and spread.js
@@ -0,0 +1,24 @@
+//* rest parameter
+// can we call a function with any number of arguments even if it isn't defined? if we passed extra arguments in a function how can we get them?
+
+// what is rest parameter syntax? how it works? must we use rest parameter at the end of parameter list?
+
+// inside a function what is 'arguments'? is it an array? how it contains all the arguments?
+
+// what is the downside of using 'arguments' instead of rest parameter?
+
+//* spread syntax
+// what is spread syntax? how it works?
+
+// what are the different styles of using spread syntax? multiple iterator, combining with normal values, merging arrays.
+
+// How spread syntax internally works for iterator?
+
+// Difference between Array.from(iterator) vs spread syntax? How can we copy an array using spread syntax?
+
+// Can we use spread syntax in object? how it works for object? can we copy an object using spread syntax?
+
+//=============================================================================================
+// Spread syntax (...) allows an iterable such as an array expression or string to be EXPANDED in places where zero or more arguments (for function calls) or elements (for array literals) are expected.
+
+// In an object literal, the spread syntax enumerates the properties of an object and adds the key-value pairs to the object being created.
