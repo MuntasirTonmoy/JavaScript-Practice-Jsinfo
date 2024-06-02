ac182dbdfb6f5a2976b04f4755b1016f37a45b6a
Tonmoy <muntasir01521566317@gmail.com>
2024-06-02 11:52:26 +0600

    edited

---
+++ function_advance/03_variable_scope_closure.js
@@ -0,0 +1,17 @@
+//* scope
+
+// what is scope?
+
+// what is block scope? if a variable declared inside a code block where will it be visible? use case of block scope.
+
+// if we declare variable inside the {} of 'if', 'for', 'while' etc. where those variable are visible?
+
+// for(let i = 5; i < 10; i++){} here we can see variable i is declared outside of block {}, where will it be visible?
+
+// What is a nested function? when we create a nested function can we access variable declared in the outer function?
+
+// When we return a nested function and use the returned function elsewhere can it still access the outer function variable? How it works?
+
+// What is lexical environment? how many parts lexical environment has? What each part do?
+
+// What lexical environment associated with the whole script?
